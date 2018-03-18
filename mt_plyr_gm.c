bb43c37bb1bc411772d251358192022956781b33
Shubh-007 <37450358+Shubh-007@users.noreply.github.com>
2018-03-18 22:14:31 +0530

    Add files via upload

---
+++ mt_plyr_gm.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<pthread.h>
+pthread_mutex_t l1,l2;
+pthread_t t1,t2,t3,t4,t5;
+int nog=2;nop=3,plcnt=0,gmcnt=0;
+
+void *stgm()
+	{if(gmcnt==0){pthread_mutex_lock(&l1);
+	gmcnt++;printf("%d",gmcnt);	
+	pthread_mutex_unlock(&l1);
+	}else printf("\nCannot start due to pre-exixting game\n");}
+void *gme()
+	{if(gmcnt>0){
+	pthread_mutex_lock(&l1);
+	gmcnt--;printf("\nGAME OVER\n");
+	pthread_mutex_unlock(&l1);
+	}}
+void *plyron()
+	{
+	pthread_mutex_lock(&l2);
+	plcnt++;
+	pthread_mutex_unlock(&l2);}
+void *plyroff()
+	{
+	pthread_mutex_lock(&l2);
+	plcnt--;
+	pthread_mutex_unlock(&l2);}
+void *mtst()
+	{if((plcnt>=3)&&(gmcnt==0))
+		{pthread_create(&t3,NULL,stgm,NULL);
+		pthread_join(t3,NULL);printf("\n Let the GAME begin\n");pthread_create(&t4,NULL,gme,NULL);
+		pthread_join(t4,NULL);}}
+int main()
+	{int i,j;
+	for(i=0;i<nop;i++)
+		{pthread_create(&t1,NULL,plyron,NULL);
+		printf("\nPlayer %d is online\n",i+1);
+		pthread_join(t1,NULL);}
+	for(j=0;j<nog;j++)
+		{
+		pthread_create(&t5,NULL,mtst,NULL);pthread_join(t5,NULL);
+		}
+	for(;i>0;i--)
+		{pthread_create(&t2,NULL,plyroff,NULL);printf("\nPlayer %d is offline\n",i);
+		pthread_join(t2,NULL);}
+	}
